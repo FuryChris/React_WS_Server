75cb9dc78f839d4912df3784c731c976838e0f33
unknown <desmont2222@gmail.com>
2021-11-03 21:28:57 +0100

    pre-deployment

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ io.on('connection', (socket) => {
   console.log('We have a new connection!!!');
 
   socket.on('join', ({ name, room }, callback) => {
-    const { error, user } = addUser({ id: 'socket.id', name, room });
+    const { error, user } = addUser({ id: socket.id, name, room });
 
     if (error) return callback({ error: 'Error ocurred' });
 
@@ -33,16 +33,32 @@ io.on('connection', (socket) => {
       .emit('message', { user: 'admin', text: `${user.name} has joined!` });
 
     socket.join(user.room);
+
+    io.to(user.room).emit('roomData', {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+
+    callback();
   });
 
   socket.on('sendMessage', (message, callback) => {
     const user = getUser(socket.id);
-
+    console.log(user);
     io.to(user.room).emit('message', { user: user.name, text: `${message}` });
     callback();
   });
 
   socket.on('disconnect', () => {
+    const user = removeUser(socket.id);
+
+    // if (user) {
+    //   io.to(user.room).emit('message', {
+    //     user: 'admin',
+    //     text: `${user.name} has left`,
+    //   });
+    // }
+    console.log(user);
     console.log('User had left!!!');
   });
 });
